f75ea0a08661a1b84a7f63964700a584c945f052
drewlew13 <lewis446@purdue.edu>
2020-02-04 15:02:18 -0500

    First commit

---
+++ pa1.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "shell_array.h"
+#include "shell_list.h"
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        return (EXIT_FAILURE); // not enough arguments for function (i.e. no i/o file)
+    }
+
+    long pos_num = 0;
+    long element_num = 0;
+    long array = NULL;
+    int size = 0;
+
+    FILE * fptr = fopen(argv[1], 'rb');
+
+    fseek(fptr, 0, SEEK_END); // Find the position of the eof and use ftell to find the amount of bytes in the file
+    pos_num = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    size = pos_num / sizeof(long); // Calculate the amount of long ints in the file
+
+    fclose(fptr);
+
+    array = Array_Load_From_File(argv[1], &size);
+}
---
+++ shell_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "shell_array.h"
+
+long *Array_Load_From_File(char* filename, int* size)
+{
+    FILE * fptr = fopen(filename, 'r');
+    long * long_arr = NULL;
+    
+    if (fptr == NULL) // Check if fopen fails
+    {
+        printf("fopen failed\n");
+        *size = 0;
+        return(long_arr);
+    }
+    long_arr = malloc((sizeof(long)) * (*size)); // Define long_arr to hold the exact amount of long ints in the file
+    
+    if (long_arr == NULL) // Check if malloc fails
+    {
+        printf("malloc failed\n")
+        *size = 0;
+    }
+    
+    if ((*size) == 0)
+    {
+        return (long_arr);
+    }
+    
+    fread(long_arr, sizeof(long), size, fptr);
+
+    return(long_arr);
+
+}
