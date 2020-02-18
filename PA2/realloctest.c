48dcc46b692e14063696b88afd29fabc50ff3ce4
drewlew13 <lewis446@purdue.edu>
2020-02-18 17:10:48 -0500

    realloc test

---
+++ PA2/realloctest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main();
+
+int main()
+{
+int * array = malloc(4 * sizeof(int));
+int i = 0;
+
+for (i = 0; i < 4; i++)
+{
+    array[i] = i;
+}
+array = realloc(array, 3 * sizeof(int));
+for (i = 0; i < 3; i++)
+{
+    printf("%d\n", array[i]);
+}
+return 0;
+}
