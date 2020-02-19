d9591df51808cf630cfc678136f7a8f782205f63
drewlew13 <lewis446@purdue.edu>
2020-02-19 18:58:21 -0500

    Updated header file

--- PA2/h_comp.h
+++ PA2/h_comp.h
@@ -26,20 +26,24 @@ List * Add_Node(List * head, char chr);
 
 void Read_Freq(char * filename, List * head);
 
-List * sort_List(List * head);
+void sort_List(List * head);
+void swap(List * node1, List * node2);
+
+long countNode(List * head);
 
 Tree * Build_Tree(TreeList * head);
 Tree * Add_TreeNode(char chr, long freq);
 Tree * Merge_Tree(Tree * node1, Tree * node2);
-Tree * createBranch(Tree * node1, Tree * node2);
+List * Free_Node(List * head, List * nodeToDelete);
+
+
+TreeList * createTLNode(Tree * root);
+TreeList * TL_Insert(TreeList * head, TreeList * node);
+TreeList * TL_Build(Tree ** treeArray, long size);
+
 
 void PreOrder_Traverse(char * filename, Tree * treenode);
 
 void PreOrder_Traverse_Code(char * filename, Tree * treenode);
 
 void Compress(char * filenamein, char * filenameout, Tree * treenode);
-
-void swap(List * node1, List * node2);
-
-List * Free_Node(List * head, List * nodeToDelete);
-List * Destroy_List(List * head);
