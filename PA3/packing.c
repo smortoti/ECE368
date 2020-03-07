e0ecd19ff9830ef00c81050fdfcf3977bfb76dfd
drewlew13 <lewis446@purdue.edu>
2020-03-07 15:18:46 -0500

    first drafts

--- PA3/packing.c
+++ PA3/packing.c
@@ -12,16 +12,13 @@ Tree * buildTreeFromPostOrder(char * filename)
         return NULL;
     }
 
-    fseek(fptr, 0, SEEK_END);
-    
-    int size = ftell(fptr);
-
-    int * readArray = malloc(sizeof(int) * size);
-    int * readTemp = 0;
-
-    fseek(fptr, 0, SEEK_SET);
-
+    int readTemp = 0;
+    List * head = NULL;
+    int position = 0;
     int i = 0;
+    int label = 0;
+    int width = 0;
+    int height = 0;
     /* Read will have form of 
         %d ( %d , %d ) \n
     or
@@ -30,14 +27,54 @@ Tree * buildTreeFromPostOrder(char * filename)
     while(!(feof(fptr)))
     {
         readTemp = fgetc(fptr);
-        if (!(feof(fptr)) && !(readTemp == '(' || readTemp == ')'))
+        if (readTemp == 'H' || readTemp == 'V')
         {
-            readArray[i] = readTemp;
-            i++;
+            head = createNode(head, readTemp, 0, 0);
         }
+        else
+        {
+            if(!(feof(fptr)))
+            {
+                position = ftell(fptr);
+                fseek(fptr, (long) (position - 1), SEEK_SET)
+                fscanf(fptr, "%d(%d, %d)\n", &label, &width, &height);
+                head = createNode(head, label, width, height);
+            }
+        }    
+    }
+    
+    fclose(fptr);
+}
+
+List * createNode(List * head, int label, int width, int height)
+{
+    if (head == NULL)
+    {
+        List * temp = malloc(sizeof(*temp));
+
+        temp -> label = label;
+        temp -> height = height;
+        temp -> width = width;
+        temp -> next = NULL;
+
+        head = temp;
+
+        return(head);
     }
 
+    List * newNode = malloc(sizeof(*newNode));
+    List * temp = head;
 
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
 
+    newNode -> label = label;
+    newNode -> height = height;
+    newNode -> width = width;
+    newNode -> next = NULL;
+    temp -> next = newNode;
 
+    return(head);
 }
--- PA3/packing.h
+++ PA3/packing.h
@@ -1,13 +1,23 @@
 typedef struct _Tree
 {
-    long name;
-    long length;
-    long width;
+    int label;
+    int height;
+    int width;
     _Tree * left;
     _Tree * right;
 } Tree;
 
+typedef struct _List
+{
+    int label;
+    int height;
+    int width;
+    _List * next;
+} List;
+
+
 Tree * buildTreeFromPostOrder(char * filename); // builds tree from given postorder traversal
+List * createNode(List * head, int label, int width, int height)
 
 void * printPreOrder(char * filename, Tree * root); // prints tree with preorder traversal
 
