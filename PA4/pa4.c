11b296b725f7c6d154757630b035eb9dbcf4caae
drewlew13 <lewis446@purdue.edu>
2020-03-29 20:20:25 -0400

    updates

--- PA4/pa4.c
+++ PA4/pa4.c
@@ -1,54 +1,60 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hbt.h"
 #include "hbtfun.h"
 
 int main(int argc, char ** argv)
 {
-    if (argc != 3)
+    char b[] = "-b";
+    char e[] = "-e";
+
+    if (!(strcmp(argv[1], b)))
     {
-        fprintf(stderr, "Incorrect number of inputs\n");
-        return EXIT_FAILURE;
-    }
+        FILE * fptr = fopen(argv[1], "rb");
 
-    FILE * fptr = fopen(argv[1], "rb");
-
-    if (fptr == NULL)
-    {
-        fprintf(stdout, "%d\n", -1);
-        return EXIT_FAILURE;
-    }
-
-    int key = 0;
-    char mode = '0';
-    Tnode * root = NULL;
-    Tnode * newNode = NULL;
-
-    while(!(feof(fptr)))
-    {
-        fread(&key, sizeof(int), 1, fptr);
-        fread(&mode, sizeof(char), 1, fptr);
-
-        if(!(feof(fptr)))
+        if (fptr == NULL)
         {
-            if (mode == 'i')
+            fprintf(stdout, "%d\n", -1);
+            return EXIT_FAILURE;
+        }
+
+        int key = 0;
+        char mode = '0';
+        Tnode * root = NULL;
+        Tnode * newNode = NULL;
+
+        while(!(feof(fptr)))
+        {
+            fread(&key, sizeof(int), 1, fptr);
+            fread(&mode, sizeof(char), 1, fptr);
+
+            if(!(feof(fptr)))
             {
-                newNode = createNode(key);
-                root = insertNode(root, newNode);
-            }
-            else
-            {
-                deleteNode(root, key);  
+                if (mode == 'i')
+                {
+                    newNode = createNode(key);
+                    root = insertNode(root, newNode);
+                }
+                else
+                {
+                    deleteNode(root, key);  
+                }
             }
         }
+
+        fprintf(stdout, "%d\n", 1);
+
+        printPreOrder(argv[2], root);
+
+        destroyTree(root);
     }
 
-    fprintf(stdout, "%d\n", 1);
-
-    printPreOrder(argv[2], root);
-
-    destroyTree(root);
+    else if (!(strcmp(argv[1], e)))
+    {
+        
+    }
 
     return EXIT_SUCCESS;
 
